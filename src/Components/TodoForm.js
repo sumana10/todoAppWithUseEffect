80d1d657b55c7fb92abdf45875e4015a4c985d59
sumana10 <sumanaonlyme@gmail.com>
2021-09-22 20:58:40 +0530

    initial commit

---
+++ src/Components/TodoForm.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react'
+
+import{
+  FormGroup,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  Button,
+  Form,
+  Container
+} from "reactstrap"
+
+import {v4} from "uuid"
+
+//methods can also be passed into the prop
+
+export default function TodoForm({addTodos}) {
+
+  const [todoString, setTodoString] = useState("");
+
+  const handleSubmit = e =>{
+
+    e.preventDefault();
+   
+    if(todoString === ""){
+    
+       return alert("Please enter todo")
+
+    }
+
+    const todo = {
+      todoString,
+      id: v4()
+    }
+   
+    addTodos(todo)
+
+    setTodoString("")
+  }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <InputGroup>
+          <Input
+          type="text"
+          name="todo"
+          id="todo"
+          placeholder="Enter a todo"
+          value={todoString}
+          onChange= {e =>setTodoString(e.target.value)}
+          />
+          <InputGroupAddon addonType="prepend">
+            <Button color="info">
+              Add Todo
+            </Button>
+          </InputGroupAddon>
+        </InputGroup>
+      </FormGroup>
+    
+    </Form>
+
+  )
+}
